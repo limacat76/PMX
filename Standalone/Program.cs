ddc3614d50acafbc9845f78b9aa642acbfaa2996
Davide "limaCAT" Inglima <limacat@live.it>
2021-01-18 16:49:02 +0100

    Add project files.

---
+++ Standalone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Standalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
